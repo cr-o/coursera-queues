d7d3a8c841f70f7a42236e3eadc9ebfd0ae47a01
catbeans <workemailzoop@gmail.com>
2019-10-29 22:01:34 -0400

    wip

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /* *****************************************************************************
  *  Name:
@@ -6,31 +7,35 @@ import java.util.Iterator;
  *  Description:
  **************************************************************************** */
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private Item first = null;
-
-    private class Node {
-        Item data;
-        Node next;
-    }
+    private Item[] randomizedQueue;
+    private int size;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
+        size = 1;
+        randomizedQueue = (Item[]) new Object[size];
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
+        return false;
     }
 
     // return the number of items on the randomized queue
     public int size() {
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        // add at beginning
+        size++;
     }
 
     // remove and return a random item
     public Item dequeue() {
+        // remove at random
+        size--;
     }
 
     // return a random item (but do not remove it)
@@ -43,11 +48,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public class ListIterator implements Iterator<Item> {
+        private int index;
+
         public boolean hasNext() {
-            return false;
+            return index < randomizedQueue.length;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("This operation is not supported");
         }
 
         public Item next() {
+            if (index >= randomizedQueue.length - 1) {
+                throw new NoSuchElementException("Cannot removeFirst, no such element exists");
+            }
+            return randomizedQueue[index + 1];
         }
     }
 
